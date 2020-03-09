29c5b8dc1742483dac632ea1c99381247010afd1
RoyShen12 <royshen12@gmail.com>
2020-03-09 15:28:26 +0800

    hambat AOE, moon spider spike

--- modmain.lua
+++ modmain.lua
@@ -1,5 +1,12 @@
 local TUNING = GLOBAL.TUNING
 local ACTIONS = GLOBAL.ACTIONS
+local PI = GLOBAL.PI
+local GetRandomWithVariance = GLOBAL.GetRandomWithVariance
+local TheSim = GLOBAL.TheSim
+local SpawnPrefab = GLOBAL.SpawnPrefab
+local TheWorld = GLOBAL.TheWorld
+local Vector3 = GLOBAL.Vector3
+local shuffleArray = GLOBAL.shuffleArray
 
 GLOBAL.c_dir = function (value, desciption, nesting)
   if type(nesting) ~= "number" then
@@ -72,33 +79,38 @@ GLOBAL.c_dir = function (value, desciption, nesting)
   end
 end
 
--- 烧饭速度
-TUNING.BASE_COOK_TIME = 1
+-- 海钓
+-- 甩杆更远
+TUNING.OCEAN_FISHING.MAX_CAST_DIST = 24
+-- 钓鱼线更难被拉断
+TUNING.OCEAN_FISHING.LINE_TENSION_HIGH = 0.97
 
--- 海星
-TUNING.STARFISH_TRAP_DAMAGE = 180
+-- 时间常量和速度修改
+-- 烧饭基准速度
+TUNING.BASE_COOK_TIME = 0.8
+-- 鼹鼠帽
+TUNING.MOLEHAT_PERISHTIME = 480 * 4.5
 
 -- 生物生成时间
 -- 地下蛤蟆
 TUNING.TOADSTOOL_RESPAWN_TIME = 480
 
--- 速度
+-- 物品修改
+-- 火腿棒
+HAMBAT_DAMAGE = 98
+HAMBAT_MIN_DAMAGE_MODIFIER = .4
 -- 步行手杖
 TUNING.CANE_SPEED_MULT = 1.44
-
 -- 打火机
 TUNING.LIGHTER_DAMAGE = 27
 TUNING.LIGHTER_FUEL = 20000
-
 -- 天气棒
 TUNING.TORNADOSTAFF_USES = 40
-
 -- 女武神
 TUNING.WATHGRITHR_HEALTH = 300
 TUNING.WATHGRITHR_HUNGER = 180
 TUNING.WATHGRITHR_SANITY = 100
 TUNING.WATHGRITHR_ABSORPTION = 0.18
-
 -- 沃尔夫冈
 TUNING.WOLFGANG_HUNGER = 300
 TUNING.WOLFGANG_START_HUNGER = 200
@@ -106,24 +118,19 @@ TUNING.WOLFGANG_START_MIGHTY_THRESH = 225
 TUNING.WOLFGANG_END_MIGHTY_THRESH = 220
 TUNING.WOLFGANG_START_WIMPY_THRESH = 100
 TUNING.WOLFGANG_END_WIMPY_THRESH = 105
-
 TUNING.WOLFGANG_HUNGER_RATE_MULT_MIGHTY = 4
 TUNING.WOLFGANG_HUNGER_RATE_MULT_NORMAL = 2
 TUNING.WOLFGANG_HUNGER_RATE_MULT_WIMPY = 1.2
-
 TUNING.WOLFGANG_HEALTH_MIGHTY = 450
 TUNING.WOLFGANG_HEALTH_NORMAL = 250
 TUNING.WOLFGANG_HEALTH_WIMPY = 150
-
 TUNING.WOLFGANG_ATTACKMULT_MIGHTY_MAX = 3.6
 TUNING.WOLFGANG_ATTACKMULT_MIGHTY_MIN = 1.5
 TUNING.WOLFGANG_ATTACKMULT_NORMAL = 1
 TUNING.WOLFGANG_ATTACKMULT_WIMPY_MAX = .75
 TUNING.WOLFGANG_ATTACKMULT_WIMPY_MIN = .4
-
 -- 薇洛
 TUNING.WILLOW_ABSORPTION = 0.05
-
 -- 女武神 长矛/头盔
 TUNING.WATHGRITHR_SPEAR_DAMAGE = 46
 TUNING.WATHGRITHR_SPEAR_USES = 600
@@ -145,6 +152,8 @@ TUNING.CHESTER_HEALTH = 800
 TUNING.CHESTER_RESPAWN_TIME = 240 -- total_day_time = 480
 
 -- 怪物加强
+-- 海星
+TUNING.STARFISH_TRAP_DAMAGE = 180
 -- 猪
 TUNING.PIG_DAMAGE = 44
 TUNING.PIG_HEALTH = 500
@@ -182,9 +191,7 @@ TUNING.MINOTAUR_HEALTH = 2500 * 8
 -- TUNING.DEER_HEALTH = 450 * 2
 -- TUNING.DEER_GEMMED_DAMAGE = 60
 -- TUNING.DEER_GEMMED_HEALTH = 2500
---
-TUNING.KLAUS_DAMAGE = 110
-TUNING.KLAUS_HEALTH = 20000
+
 -- 蜘蛛
 TUNING.SPIDER_HEALTH = 150
 TUNING.SPIDER_DAMAGE = 30
@@ -196,32 +203,6 @@ TUNING.SPIDER_MOON_HEALTH = 500
 TUNING.SPIDER_MOON_DAMAGE = 40
 TUNING.SPIDERQUEEN_HEALTH = 5000
 TUNING.SPIDERQUEEN_DAMAGE = 105
--- 石虾
-TUNING.ROCKY_DAMAGE = 100
-TUNING.ROCKY_HEALTH = 6000
--- Boss
--- 树人
-TUNING.LEIF_HEALTH = 5000
-TUNING.LEIF_DAMAGE = 200
--- 巨鹿
-TUNING.DEERCLOPS_HEALTH = 8000
-TUNING.DEERCLOPS_DAMAGE = 235
--- 鹅
-TUNING.MOOSE_HEALTH = 15000
-TUNING.MOOSE_DAMAGE = 200
--- 蜜蜂女王
-TUNING.BEEQUEEN_HEALTH = 35000
-TUNING.BEEQUEEN_DAMAGE = 225
--- 蜂巢卫士
-TUNING.BEEGUARD_HEALTH = 400
-TUNING.BEEGUARD_DAMAGE = 35
--- 龙蝇
-TUNING.DRAGONFLY_HEALTH = 100000
-TUNING.DRAGONFLY_DAMAGE = 330
-TUNING.DRAGONFLY_FIRE_DAMAGE = 425
--- 复活的骨架（远古编织者）
-TUNING.STALKER_ATRIUM_HEALTH = 32000
-TUNING.STALKER_ATRIUM_PHASE2_HEALTH = 20000
 -- 地下蠕虫
 TUNING.WORM_HEALTH = 1200
 -- 各种狗
@@ -235,9 +216,48 @@ TUNING.WORM_HEALTH = 1200
 -- TUNING.MOONHOUND_DAMAGE = 20
 -- TUNING.MUTATEDHOUND_HEALTH = 100
 -- TUNING.MUTATEDHOUND_DAMAGE = 25
-
 -- 坎普斯
 TUNING.KRAMPUS_HEALTH = 500
+-- 石虾
+TUNING.ROCKY_DAMAGE = 100
+TUNING.ROCKY_HEALTH = 6000
+
+-- Boss
+-- 克劳斯
+TUNING.KLAUS_HEALTH = 50000
+TUNING.KLAUS_DAMAGE = 155
+-- 犀牛
+TUNING.MINOTAUR_DAMAGE = 270
+TUNING.MINOTAUR_HEALTH = 2500 * 12
+-- 树人
+TUNING.LEIF_HEALTH = 5000
+TUNING.LEIF_DAMAGE = 200
+-- 巨鹿
+TUNING.DEERCLOPS_HEALTH = 12000
+TUNING.DEERCLOPS_DAMAGE = 275
+-- 鹅
+TUNING.MOOSE_HEALTH = 25000
+TUNING.MOOSE_DAMAGE = 220
+-- 熊獾
+TUNING.BEARGER_HEALTH = 30000
+TUNING.BEARGER_DAMAGE = 355
+-- 蜜蜂女王
+TUNING.BEEQUEEN_HEALTH = 35000
+TUNING.BEEQUEEN_DAMAGE = 335
+-- 蜂巢卫士
+TUNING.BEEGUARD_HEALTH = 400
+TUNING.BEEGUARD_DAMAGE = 35
+-- 龙蝇
+TUNING.DRAGONFLY_HEALTH = 100000
+TUNING.DRAGONFLY_DAMAGE = 445
+TUNING.DRAGONFLY_FIRE_DAMAGE = 625
+-- 复活的骨架（远古编织者）
+TUNING.STALKER_ATRIUM_HEALTH = 32000
+TUNING.STALKER_ATRIUM_PHASE2_HEALTH = 20000
+-- 信天翁
+TUNING.MINOTAUR_HEALTH = 20000
+
+
 
 -- 添加突进
 -- 武器具备 blinking 属性时触发
@@ -247,10 +267,12 @@ AddPlayerPostInit(function(inst)
 	if inst.components.combat == nil then
 		return
 	end
-	if GLOBAL.TheWorld.ismastersim and inst.prefab == "wathgrithr" then
+	if TheWorld.ismastersim and inst.prefab == "wathgrithr" then
 		local old_start = inst.components.combat.StartAttack
+
 		inst.components.combat.StartAttack = function(self)
 			old_start(self)
+
 			if self.target and self.target.components.combat then
 				local weapon = self:GetWeapon()
 				if weapon and weapon.components.weapon and weapon.components.weapon.blinking then
@@ -264,26 +286,28 @@ AddPlayerPostInit(function(inst)
 							local dy = sy - q * (sy - fy)
 							local dz = sz - q * (sz - fz)
 							-- 不会突进到水中
-							if GLOBAL.TheWorld.Map:IsOceanTileAtPoint(dx, dy, dz) then
+							if TheWorld.Map:IsOceanTileAtPoint(dx, dy, dz) then
 								return
 							end
-							local smoke1 = GLOBAL.SpawnPrefab("maxwell_smoke")
+							-- 生成烟雾
+							local smoke1 = SpawnPrefab("maxwell_smoke")
 							smoke1.Transform:SetPosition(sx, sy, sz)
-							local smoke2 = GLOBAL.SpawnPrefab("maxwell_smoke")
+							local smoke2 = SpawnPrefab("maxwell_smoke")
 							smoke2.Transform:SetPosition(dx, dy, dz)
 							inst.Transform:SetPosition(dx, dy, dz)
-							-- 突进自身的伤害
+							-- 突进瞬间造成伤害
 							local blinking_damage = weapon.components.weapon.damage * (weapon.components.weapon.blinking_damage_multiplier or 1.75)
 							self.target.components.combat:GetAttacked(self.inst, blinking_damage, weapon)
-							-- 对小范围的额外伤害
+							-- 对小范围的额外 AOE 伤害
 							if weapon.components.weapon.blinking_with_aoe then
-								local ents = GLOBAL.TheSim:FindEntities(dx, dy, dz, (weapon.components.weapon.blinking_aoe_radius or 2))
+								local ents = TheSim:FindEntities(dx, dy, dz, (weapon.components.weapon.blinking_aoe_radius or 2))
 								for k, v in pairs(ents) do
 									if inst.components.combat:IsValidTarget(v)
 										 and v ~= self.target and v.components.combat
+										 -- 不会对墙体造成 AOE
 										 and string.find(v.prefab, "wall") == nil then
 										-- print("blinking aoe to ", v)
-										v.components.combat:GetAttacked(inst, weapon.components.weapon.blinking_aoe_damage or blinking_damage * 0.5, weapon)
+										v.components.combat:GetAttacked(inst, weapon.components.weapon.blinking_aoe_damage or (blinking_damage * 0.5), weapon)
 									end
 								end
 							end
@@ -298,7 +322,7 @@ local function custom_willow_sanityfn(inst)
 	local delta = inst.components.temperature:IsFreezing() and -TUNING.SANITYAURA_MED or 0
 	local x, y, z = inst.Transform:GetWorldPosition()
 	local max_rad = 10
-	local ents = GLOBAL.TheSim:FindEntities(x, y, z, max_rad, { "fire" })
+	local ents = TheSim:FindEntities(x, y, z, max_rad, { "fire" })
 	for i, v in ipairs(ents) do
 			if v.components.burnable ~= nil and v.components.burnable:IsBurning() then
 					local rad = v.components.burnable:GetLargestLightRadius() or 1
@@ -312,15 +336,16 @@ local function custom_willow_sanityfn(inst)
 end
 
 AddPrefabPostInit("willow", function (inst)
-	if GLOBAL.TheWorld.ismastersim then
+	if TheWorld.ismastersim then
 		-- 移除纵火者（烧家）特性
 		-- inst:RemoveTag("pyromaniac")
-		-- 内秉的伤害吸收
+
+		-- 添加内秉的伤害吸收
 		inst.components.health:SetAbsorptionAmount(TUNING.WILLOW_ABSORPTION)
 
 		inst.components.sanity.custom_rate_fn = custom_willow_sanityfn
-		-- 更耐饿
-		inst.components.hunger.hungerrate = 0.8 * TUNING.WILSON_HUNGER_RATE
+		-- 薇洛饥饿更慢
+		inst.components.hunger.hungerrate = 0.89 * TUNING.WILSON_HUNGER_RATE
 
 		inst.components.locomotor.runspeed = 12
 		inst.components.locomotor.walkspeed = 7
@@ -329,37 +354,74 @@ AddPrefabPostInit("willow", function (inst)
 	end
 end)
 
+local variations = {1, 2, 3, 4, 5}
+local function DoSpikeAttack(inst, centerTarget)
+	local x, y, z = centerTarget.Transform:GetWorldPosition()
+	local inital_r = 1
+	x = GetRandomWithVariance(x, inital_r)
+	z = GetRandomWithVariance(z, inital_r)
+
+	shuffleArray(variations)
+
+	local num = math.random(2, 4)
+	local dtheta = PI * 2 / num
+	local thetaoffset = math.random() * PI * 2
+	local delaytoggle = 0
+	for i = 1, num do
+		local r = 1.1 + math.random() * 1.75
+		local theta = i * dtheta + math.random() * dtheta * 0.8 + dtheta * 0.2
+			local x1 = x + r * math.cos(theta)
+			local z1 = z + r * math.sin(theta)
+			if TheWorld.Map:IsVisualGroundAtPoint(x1, 0, z1) and not TheWorld.Map:IsPointNearHole(Vector3(x1, 0, z1)) then
+				local spike = SpawnPrefab("moonspider_spike")
+				spike.components.combat:SetDefaultDamage(inst.components.weapon.damage or 100)
+				spike.Transform:SetPosition(x1, 0, z1)
+				spike:SetOwner(inst)
+
+				if variations[i + 1] ~= 1 then
+					spike.AnimState:OverrideSymbol("spike01", "spider_spike", "spike0"..tostring(variations[i + 1]))
+				end
+			end
+	end
+end
+
 local function custom_lighter_onattack(weapon, attacker, target)
+	attacker.SoundEmitter:PlaySound("dontstarve/wilson/fireball_explo")
+
+	-- 只有薇洛可以触发打火机的蓄势
 	if target ~= nil and target:IsValid() and attacker.prefab == "willow" then
+		-- 蓄势
 		weapon.components.weapon.updateXuShi(weapon.components.weapon, attacker)
-		-- if weapon.components.weapon.damage <= 155 then
-		-- 	local debug_old_damage = weapon.components.weapon.damage
-		-- 	local newDamage = math.floor(weapon.components.weapon.damage * 1.015 + 0.5)
-		-- 	attacker.components.talker:Say("武器伤害提高了 " .. (newDamage - debug_old_damage))
-		-- 	weapon.components.weapon:SetDamage(newDamage)
-		-- 	attacker.components.sanity:DoDelta(math.random(1, 3))
-		-- end
-		-- print("new damage:", weapon.components.weapon.damage)
-		if target.components.burnable ~= nil and math.random() < TUNING.LIGHTER_ATTACK_IGNITE_PERCENT * target.components.burnable.flammability and attacker.prefab == "willow" then
-			-- target.components.burnable:Ignite(nil, attacker)
-			target.components.combat:GetAttacked(attacker, weapon.components.weapon.damage * 2, weapon)
-			attacker.components.sanity:DoDelta(math.random(2, 6))
-			attacker.components.health:DoDelta(math.random(1, 3))
+
+		if target.components.burnable ~= nil
+			 and math.random() < TUNING.LIGHTER_ATTACK_IGNITE_PERCENT * target.components.burnable.flammability then
+			-- target.components.burnable:Ignite(nil, attacker) -- 原生的点燃已被移除
+			target.components.combat:GetAttacked(attacker, weapon.components.weapon.damage * 1.25, weapon)
+			-- attacker.components.sanity:DoDelta(math.random(2, 6))
+			-- attacker.components.health:DoDelta(math.random(1, 3))
+		end
+
+		if math.random() < 1 then
+			DoSpikeAttack(attacker, target)
 		end
 	end
 end
 
 AddPrefabPostInit("lighter", function (inst)
-	if GLOBAL.TheWorld.ismastersim then
+	if TheWorld.ismastersim then
+
 		inst.components.weapon:SetOnAttack(custom_lighter_onattack)
 		inst.components.weapon:SetRange(5, 7)
 		inst.components.weapon:SetProjectile("fire_projectile")
 		inst.components.weapon.xushi = 0
 		-- 蓄势 18% @ x5
+		-- 15层
 		inst.components.weapon.updateXuShi = (function ()
-			local xushiCengShu = 25
+			local xushiCengShu = 15
+			local recoverXuShi = math.floor(0.5 + xushiCengShu * 0.8)
 			local timmerInst = nil
 			return function (_inst, attacker)
+
 				if (_inst.xushi < xushiCengShu) then
 					_inst.xushi = _inst.xushi + 1
 					local newDamage = math.floor(0.5 + TUNING.LIGHTER_DAMAGE * math.pow(1.18, _inst.xushi))
@@ -368,16 +430,19 @@ AddPrefabPostInit("lighter", function (inst)
 				elseif (_inst.xushi == xushiCengShu) then
 					attacker.components.talker:Say("蓄势 [ " .. _inst.xushi .. " ] 武器伤害: " .. _inst.damage)
 				end
+
 				if timmerInst ~= nil then timmerInst:Cancel() end
+
 				timmerInst = inst:DoTaskInTime(5, function ()
 					_inst.xushi = 0
 					_inst:SetDamage(TUNING.LIGHTER_DAMAGE * 1)
 					attacker.components.talker:Say("蓄势归零")
 				end)
-				-- recover sanity
-				attacker.components.sanity:DoDelta(math.random(1, 8) / 10 * _inst.xushi)
-				if _inst.xushi == xushiCengShu then
-					attacker.components.health:DoDelta(5)
+
+				-- 蓄势层数 > 80% 最大层数 时恢复精神和生命值
+				if _inst.xushi >= recoverXuShi then
+					attacker.components.sanity:DoDelta(math.random(1, 2))
+					attacker.components.health:DoDelta(math.random(1, 5))
 				end
 			end
 		end)()
@@ -388,16 +453,23 @@ AddPrefabPostInit("lighter", function (inst)
 end)
 
 AddPrefabPostInit("wathgrithr", function (inst)
-	if GLOBAL.TheWorld.ismastersim then
+	if TheWorld.ismastersim then
+
+		if inst.components.combat then
+			-- 女武神空手 2 倍伤害
+			inst.components.combat.defaultdamage = TUNING.UNARMED_DAMAGE * 2
+		end
+
 		inst.components.locomotor.runspeed = 9
 		inst.components.locomotor.walkspeed = 5
-
-		inst.components.hunger.hungerrate = 1.5 * TUNING.WILSON_HUNGER_RATE
+		-- 女武神饥饿更快
+		inst.components.hunger.hungerrate = 1.88 * TUNING.WILSON_HUNGER_RATE
 	end
 end)
 
 AddPrefabPostInit("spear_wathgrithr", function (inst)
-	if GLOBAL.TheWorld.ismastersim then
+	if TheWorld.ismastersim then
+
 		inst.components.weapon.blinking = true
 		inst.components.weapon.blinking_damage_multiplier = 2.2
 		inst.components.weapon.blinking_with_aoe = true
@@ -410,20 +482,29 @@ AddPrefabPostInit("spear_wathgrithr", function (inst)
 		inst.components.equippable:SetOnEquip(function (_inst, owner)
 			oldOnEquip(_inst, owner)
 			if owner.prefab == "wathgrithr" then
-				owner.components.combat:SetAttackPeriod(TUNING.WILSON_ATTACK_PERIOD * 0.7)
-				owner.components.talker:Say("装备专属长矛，攻击速度提高 43%")
+				-- owner.components.combat:SetAttackPeriod(TUNING.WILSON_ATTACK_PERIOD * 0.7)
+				-- inst.components.weapon:SetDamage(TUNING.WATHGRITHR_SPEAR_DAMAGE * 1.46)
+				owner.components.combat.damagemultiplier = TUNING.WATHGRITHR_DAMAGE_MULT * 1.46
+				owner.components.talker:Say("装备专属长矛，伤害提高 46%")
 			end
 		end)
 		inst.components.equippable:SetOnUnequip(function (_inst, owner)
 			oldOnUnEquipfn(_inst, owner)
 			if owner.prefab == "wathgrithr" then
-				owner.components.combat:SetAttackPeriod(TUNING.WILSON_ATTACK_PERIOD)
-				owner.components.talker:Say("卸下专属长矛，攻击速度减少 43%")
+				-- owner.components.combat:SetAttackPeriod(TUNING.WILSON_ATTACK_PERIOD)
+				-- inst.components.weapon:SetDamage(TUNING.WATHGRITHR_SPEAR_DAMAGE)
+				owner.components.combat.damagemultiplier = TUNING.WATHGRITHR_DAMAGE_MULT
+				owner.components.talker:Say("卸下专属长矛，伤害减少 46%")
 			end
 		end)
 
 		inst.components.weapon:SetOnAttack(function (weapon, attacker, target)
-			if attacker.prefab == "wathgrithr" and target and target.components.combat and target.components.combat.defaultdamage then
+			if attacker.prefab == "wathgrithr"
+				 and target
+				 and target.components.combat
+				 and target.components.combat.defaultdamage
+				 and target.components.health
+				 and target.components.health.maxhealth > 0 then
 
 				local extraDamageMutl = 0
 
@@ -434,34 +515,93 @@ AddPrefabPostInit("spear_wathgrithr", function (inst)
 					extraDamageMutl = 1
 				elseif attacker.components.health:GetPercent() < 0.5 then
 					extraDamageMutl = 0.5
+				elseif attacker.components.health:GetPercent() < 0.8 then
+					extraDamageMutl = 0.2
 				end
 
-				target.components.combat:GetAttacked(attacker, weapon.components.weapon.damage * extraDamageMutl, weapon)
+				if extraDamageMutl > 0 then
+					target.components.combat:GetAttacked(attacker, weapon.components.weapon.damage * extraDamageMutl, weapon)
+				end
 
-				local delta = target.components.combat.defaultdamage
-				local healthStealMult = 0.02
+				local targetStrength = target.components.combat.defaultdamage + target.components.health.maxhealth / 500
+				local healthStealMult = 0.01
+				-- Boss: 300+200=500
+				-- monster: 50+2=52
 
+				-- 生命低于 30% 吸血增加
 				if attacker.components.health:GetPercent() < 0.3 then
-					healthStealMult = healthStealMult + 0.08
+					healthStealMult = healthStealMult + 0.04
 				end
+				-- 史诗生物额外吸血
 				if target:HasTag("epic") or math.random() < 0.1 then
-					healthStealMult = healthStealMult * 3
+					healthStealMult = healthStealMult * 2
 				end
-				attacker.components.health:DoDelta(delta * healthStealMult)
-				attacker.components.sanity:DoDelta(delta * healthStealMult / 8)
+				attacker.components.health:DoDelta(targetStrength * healthStealMult)
+				attacker.components.sanity:DoDelta(targetStrength * healthStealMult / 15)
 			end
 		end)
+
 		-- 多用工具
 		inst:AddComponent("tool")
 		inst.components.tool:SetAction(ACTIONS.CHOP, 8)
+
 		inst.components.finiteuses:SetConsumption(ACTIONS.CHOP, 1)
+
 		inst.components.equippable.walkspeedmult = 1.05
+
+		--瞬移杖效果
+		inst:AddComponent("blinkstaff")
+		inst.components.blinkstaff:SetFX("sand_puff_large_front", "sand_puff_large_back")
+    inst.components.blinkstaff.onblinkfn = function (staff, pos, caster)
+			if caster.components.sanity ~= nil and caster.components.hunger ~= nil then
+					caster.components.sanity:DoDelta(-25)
+					caster.components.hunger:DoDelta(-50)
+			end
+			if staff.components.finiteuses ~= nil then
+				staff.components.finiteuses:Use(10)
+			end
+		end
 	end
 end)
 
--- AddPrefabPostInit("wathgrithrhat", function (inst)
--- 	if GLOBAL.TheWorld.ismastersim then
--- 		inst:AddComponent("waterproofer")
--- 		inst.components.waterproofer:SetEffectiveness(TUNING.WATERPROOFNESS_SMALL)
--- 	end
--- end)
+AddPrefabPostInit("wathgrithrhat", function (inst)
+	if TheWorld.ismastersim then
+		if inst.components.waterproofer then
+			inst.components.waterproofer:SetEffectiveness(TUNING.WATERPROOFNESS_SMALL * 2)
+		end
+
+		inst.components.equippable.walkspeedmult = 1.1
+	end
+end)
+
+AddPrefabPostInit("armorruins", function (inst)
+	if TheWorld.ismastersim then
+		inst.components.equippable.walkspeedmult = 1.1
+	end
+end)
+
+AddPrefabPostInit("hambat", function (inst)
+	if TheWorld.ismastersim then
+
+		local old_onattack_fn = inst.components.weapon.onattack
+		inst.components.weapon.onattack = function(weapon, attacker, target)
+			old_onattack_fn(weapon, attacker, target)
+
+			if target and attacker.components.combat:IsValidTarget(target) then
+				local x, y, z = target.Transform:GetWorldPosition()
+				local ents = TheSim:FindEntities(x, y, z, 4)
+					for k, v in pairs(ents) do
+						if attacker.components.combat:IsValidTarget(v)
+								and v ~= target and v.components.combat
+								-- 不会对墙体造成 AOE
+								and string.find(v.prefab, "wall") == nil then
+							-- print("blinking aoe to ", v)
+							v.components.combat:GetAttacked(attacker, attacker.components.combat:CalcDamage(v, weapon, 1.8), weapon)
+						end
+					end
+			end
+		end
+
+		inst.components.equippable.walkspeedmult = 1.1
+	end
+end)
